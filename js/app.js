bbb56e693efa8c3f0462b63d3a2f69b3952ea88d
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2016-01-18 19:40:04 -0800

    got images to display and counter to work but still need to work out a way to refresh without clicking refresh

--- js/app.js
+++ js/app.js
@@ -1,17 +1,18 @@
 'use strict'
 
+var bigCounter = 0;
 var productCollection = [];
-var products = ['bag', 'banana', 'boots', 'chair', 'cthulhu', 'dragon', 'pen', 'scissors', 'shark', 'sweep', 'unicorn', 'usb-1', 'water-can', 'wine-glass'];
-function RateTheseImages(name, src) {
+var products = ['bag', 'banana', 'boots', 'chair', 'cthulhu', 'dragon', 'pen', 'scissors', 'shark', 'sweep', 'unicorn', 'usb', 'water-can', 'wine-glass'];
+function RateTheseImages(name) {
   this.name = name;
-  this.filepath = 'img/' + src + '.jpg';
+  this.filepath = 'img/' + name + '.jpg';
   this.clicks = 0;
 }
 for (var i = 0; i < products.length; i++) { //makes objects
-  productCollection.push(new RateTheseImages(products[i], products[i]));
+  productCollection.push(new RateTheseImages(products[i]));
 }
 var generateRandom = function() {
-  return Math.floor(Math.random() * (15));
+  return Math.floor(Math.random() * (products.length));
 }
 var threeRandoms = function() {
   var threeNumbers = [0, 0, 0];
@@ -23,33 +24,34 @@ var threeRandoms = function() {
   return threeNumbers;
 }
 var x = threeRandoms();
-console.log(x);
-console.log(productCollection[x[0]].filepath);
-console.log(productCollection[x[1]].filepath);
-console.log(productCollection[x[2]].filepath);
-
+console.log(x)
 
 var first = document.getElementById('container1'); //dom IDs
+first.src = productCollection[x[0]].filepath;
 var second = document.getElementById('container2');
+second.src = productCollection[x[1]].filepath;
 var third = document.getElementById('container3');
+third.src = productCollection[x[2]].filepath;
 
 first.addEventListener('click', handleClickFirst); //event listeners
 second.addEventListener('click', handleClickSecond);
 third.addEventListener('click', handleClickThird);
 
-var firstClicks = 0;
-var secondClicks = 0;
-var thirdClicks = 0;
-
-function handleClickFirst() {
-  firstClicks += 1;
-  productCollection[x[0]]
+function handleClickFirst(event) {
+  console.log(event);
+  var first = document.getElementById('container1');
+  productCollection[x[0]].clicks += 1;
+  bigCounter += 1;
 }
-function handleClickSecond() {
-  secondClicks += 1;
-  productCollection[x[1]];
+function handleClickSecond(event) {
+  console.log(event);
+  var second = document.getElementById('container1');
+  productCollection[x[1]].clicks += 1;
+  bigCounter += 1;
 }
-function handleClickThird() {
-  thirdClicks += 1;
-  productCollection[x[2]]
+function handleClickThird(event) {
+  console.log(event);
+  var third = document.getElementById('container3');
+  productCollection[x[2]].clicks += 1;
+  bigCounter += 1;
 }
